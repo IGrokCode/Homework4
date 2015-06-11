f51542d211820633e2f19d1d595beb29391db282
Matt <mabercr4@uncc.edu>
2015-06-10 20:04:54 -0400

    Initial Checkin of Homework4

---
+++ 2215Homework4/src/Driver.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Driver {
+	public static void main(String [] args){
+		Scanner keys = new Scanner(System.in);
+		System.out.println("Enter n values to calculate of f(n): ");
+		long size = keys.nextLong();
+		//System.out.println("Enter max time for program to run(seconds): ");
+		//100long runTime = keys.nextLong();
+		long startTime = System.currentTimeMillis();
+		// ... do something ...
+		
+		long a = 1, b = 2, c = 7;
+		long current = 0;
+		long i=0;
+		
+		for (i=4; i<=size ;i++){
+			current = a*b-c;
+			a=b; b=c; c=current;
+			//System.out.println("f(" + i + ") = " + current);
+			//38500 calculations a second, 10^6 = 26 seconds while printing each
+		}
+		long estimatedTime = System.currentTimeMillis() - startTime;
+		System.out.println("f(" + (i-1) + ") = " + current);
+		System.out.println("Run Time: " + estimatedTime/1000 + " seconds");
+	}
+}
